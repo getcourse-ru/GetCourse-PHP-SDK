e333e8fdf9d825c419506e8e0ff99811cc028630
ds <azatonskiy@gmail.com>
2016-08-11 16:03:02 +0300

    fix

--- sample/useradd.php
+++ sample/useradd.php
@@ -3,13 +3,16 @@ include 'bootstrap.php';
 
 $user = new \GetCourse\User();
 
+$user::setAccountName('test');
+$user::setAccessToken($key);
+
 try {
 	$result = $user
-		->setEmail('vasiliy.pupkin@getcourse.ru')
+	/*	->setEmail('vasiliy.pupkin@getcourse.ru')
 		->setFirstName('Василий')
 		->setLastName('Пупкин')
 		->setOverwrite()
-		->setSessionReferer('http://getcourse.ru')
+		->setSessionReferer('http://getcourse.ru')*/
 		->apiCall($action = 'add');
 } catch (Exception $e) {
 	echo $e->getMessage();
